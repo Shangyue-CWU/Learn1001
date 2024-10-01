699af615e3391a232212afd96a09afd9defc4392
Shangyue-CWU <zhush@cwu.edu>
2024-10-01 15:39:53 -0700

    this is the first commit.

---
+++ app/src/main/java/com/example/learn1001/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.learn1001;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    TextView textView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textView1 = findViewById(R.id.textView);
+        button1 = findViewById(R.id.button1);
+
+    }
+
+    public void UpdateText(View view){
+        textView1.setText("Hello ABC110");
+        System.out.println("Hello CS460AA");
+
+    }
+}
